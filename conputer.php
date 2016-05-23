26e74566b3fc62627d37687c37b1ede3051c6271
shoko irie <shoko224335@gmail.com>
2016-05-23 19:17:01 +0900

    2回目更新

--- conputer.php
+++ conputer.php
@@ -1,25 +1,53 @@
 <html>
 <head>
-<title>calcurator</title>
+    <title>電卓</title>
 </head>
-
 <body>
-<form method="POST">
+<!--フォームを作る-->
+<form method="GET" action="conputer.php">
     計算：
-    
-    <input type="text" name="calcurator" size="3">
-    <select name="sisoku">
+    <input type="text" name="calculator" size="3">
+    <select name="operator">
         <option value="1">＋</option>
         <option value="2">－</option>
         <option value="3" selected>×</option>
         <option value="4" selected>÷</option>
     </select>
-    <input type="text" name="calcurator" size="3">
+    <input type="text" name="calculator2" size="3">
     ＝
-    <input type="submit" value="計算">
+    <input type="submit" value="計算"></form>
+    <br/>
+    <?php
+    print '計算結果　<br/>';
+
+//フォームに入力、選択されたものと対応させる
+//PHPでPOSTしたデータを受け取る
+    if(isset($_GET["calculator"]) && $_GET["calculator2"] && $_GET["operator"]) {
+        $number1 = $_GET["calculator"];
+        $number2 = $_GET["calculator2"];
+        $operand = $_GET["operator"];
+    };
+
+     //$number1 = "5";
+    //$number2 = "10";
+
+    //入力されたものを計算して、表示する
+        if (isset($operand)) {
+            if ($operand == "1") {
+                print $number1 + $number2;
+            } elseif ($operand == "2") {
+                print $number1 - $number2;
+            } elseif ($operand == "3") {
+                print $number1 * $number2;
+            } else{print $number1 / $number2;}
+        };
+
+
+?>
+
+
 </body>
 </html>
-
 
 
 
