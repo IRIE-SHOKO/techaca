28005adf4d7af989fdf77886ac94fd5eea220688
shoko irie <shoko224335@gmail.com>
2016-06-28 21:51:02 +0900

    6/28更新

--- insert_process.php
+++ insert_process.php
@@ -6,15 +6,26 @@ try {
     $db = getDb();
     //INSERT命令の準備
 
-    $stt = $db->prepare('INSERT INTO board ("name", "contents") VALUES(":name", ":contents")');
+    $stt = $db->prepare('INSERT INTO builtinboard ("name", "contents") VALUES(":name", ":contents")');
     //INSERT命令にポストデータの内容をセット
     $stt->bindValue(':name', $_POST['name']);
     $stt->bindValue(':contents', $_POST['contents']);
-    $db = NULL;
+    //実行
+    $stt->execute();
+
+    $result= $db->prepare('SELECT * from builtinboard ORDER BY id DESC');
+    $result->execute();
+
+        while ($row = $stt->fetch(PDO::FETCH_ASSOC)){
+                echo $row['name'];
+                echo"<br>";
+                echo $row['contents'];
+                echo"<br><br>";
+        };
+
 }   catch (PDOException $e){
     die("エラーメッセージ:{$e->getMessage()}");
-
-}
+    }
 
 //heade('Location: localhost/'.$_SERVER['HTTP_HOST'])
     //.dirname($_SERVER['PHP_SELF']).'/insert_form.php';
