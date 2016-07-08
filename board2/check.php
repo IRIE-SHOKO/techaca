5b5aa451a2425aa6783327ce7083049b8d0d1da3
shoko irie <shoko224335@gmail.com>
2016-07-09 00:55:46 +0900

    2016.07.09 0:55更新

--- board2/check.php
+++ board2/check.php
@@ -8,23 +8,24 @@
 session_start();
 //データベースに接続する
 require_once 'dbconnect.php';
-//直接check.phpが呼び出された場合は、入力画面("index.php")に移動させる。
-if (!isset($_POST)){
-    header('Location: join.index.php');
-    exit();
-}
 
-try {
+
+//直接check.phpが呼び出された場合は、入力画面("index.php")に移動させる。
+try{if (!isset($_POST)){
+   header('Location: join.index.php');
+   exit();
+   }
+
         $db = getDb();
         //$_POSTの中身が空ではない場合、情報をデータベースに登録する
-        $stt = $db->prepare('INSERT INTO members (name, id, password) VALUES(:name, :id, :password)');
+        $stt = $db->prepare('INSERT INTO members (name, user_id, password) VALUES(:name, :user_id, :password)');
         $stt->bindValue(':name', $_SESSION['join']['name']);
-        $stt->bindValue(':id', $_SESSION['join']['id']);
+        $stt->bindValue(':user_id', $_SESSION['join']['user_id']);
         $stt->bindValue(':password', $_SESSION['join']['password']);
         $stt->execute();
-}   catch (PDOException $e){
-    die("エラーメッセージ:{$e->getMessage()}");
-    }
+}       catch (PDOException $e){
+        die("エラーメッセージ:{$e->getMessage()}");
+        }
 ?>
 
 <form action="thanks.php" method="post">
@@ -36,7 +37,7 @@ try {
             <br>
             <dt>ユーザーID</dt>
             <dd>
-            <?php echo htmlspecialchars($_SESSION['join']['id'], ENT_QUOTES, 'UTF-8');?>
+            <?php echo htmlspecialchars($_SESSION['join']['user_id'], ENT_QUOTES, 'UTF-8');?>
             </dd>
             <br>
             <dt>パスワード</dt>
--- board2/join.index.php
+++ board2/join.index.php
@@ -6,42 +6,65 @@
 <body>
 <?php
 session_start();
+//データベースに接続
+require_once 'dbconnect.php';
 
-//POSTしたものを受け取り、それぞれの関数に格納
-if(isset($_POST["name"]) && isset($_POST["id"]) && isset($_POST["password"])) {
-    $name = $_POST["name"];
-    $id = $_POST["id"];
-    $password = $_POST["password"];
-//フォームが送信されているか確認
-//送信されている場合、次の処理に進む
-    if (!empty($_POST)) {
-        //送信されていた場合、それぞれのフォーム欄が空白になっていないか、パスワードは4文字以上か確認する
-        if ($name == '') {
-            $error['name'] = 'blank';
-        }
-        if ($id == '') {
-            $error['id'] = 'blank';
-        }
-        if (strlen($id) < 4) {
-            $error['password'] = 'length';
-        }
-        if ($password == '') {
-            $error['password'] = 'blank';
-        }
-        //エラー項目がない場合、セッション情報をcheck.phpに送信
-        if (empty($error)) {
-            $_SESSION['join'] = $_POST;
-            $_SESSION['name'] = $name;
-            $_SESSION['id'] = $id;
-            $_SESSION['password'] = $password;
-            header('Location: check.php');
-            exit();
+try {
+     //データベースへの接続を確立
+     $db = getDb();
+
+    //POSTしたものを受け取り、それぞれの関数に格納
+    if (isset($_POST['name']) && isset($_POST['user_id']) && isset($_POST['password'])) {
+        $name = $_POST['name'];
+        $user_id = $_POST['user_id'];
+        $password = $_POST['password'];
+        //フォームが送信されているか確認
+        //送信されている場合、次の処理に進む
+        if (!empty($_POST)) {
+            //送信されていた場合、それぞれのフォーム欄が空白になっていないか、パスワードは4文字以上か確認
+            if ($name == '') {
+                $error['name'] = 'blank';
+            }
+            if ($user_id == '') {
+                $error['user_id'] = 'blank';
+            }
+            if (strlen($password) < 4) {
+                $error['password'] = 'length';
+            }
+            if ($password == '') {
+                $error['password'] = 'blank';
+            }
+
+            //ユーザーIDが重複していないか確認
+            if (empty($error)) {
+                $result = $db->prepare("SELECT user_id FROM members WHERE user_id = '$user_id'");
+                $result->execute();
+                //選択された行数を返す
+                $count = $result->rowCount();
+                if ($count > 0){
+                $error['user_id'] = 'duplicate';
+                }
+            }
+
+            //エラー項目がない場合、セッション情報をcheck.phpに送信
+            if (empty($error)) {
+                $_SESSION['join'] = $_POST;
+                $_SESSION['name'] = $name;
+                $_SESSION['user_id'] = $user_id;
+                $_SESSION['password'] = $password;
+                header('Location: check.php');
+                exit();
+            }
         }
     }
-}
+}catch (PDOException $e){
+ die("エラーメッセージ:{$e->getMessage()}");
+ }
 ?>
 
 <!--入力フォームを作成-->
+<!--入力内容を常に再現-->
+<!--エラー項目をユーザーに知らせる-->
 <p>次のフォームに必要事項をご記入ください。</p>
 <form method="post" action=''>
     <dl>
@@ -51,39 +74,53 @@ if(isset($_POST["name"]) && isset($_POST["id"]) && isset($_POST["password"])) {
                        echo htmlspecialchars($name, ENT_QUOTES, 'UTF-8');
                    }?>"/>
 
-            　<?php if (isset($error['name'])){
+            <!--名前が入力されていない場合、エラーメッセージを出力-->
+            <?php if (isset($error['name'])){
                 if($error['name'] == 'blank') :?>
                     　<p class="error">*お名前を入力してください</p>
                     　<?PHP endif;} ?>
         </dd>
-        <br>
-        <dt>ユーザID<span class="required">【必須】</span></dt>
-        <dd><input type="text" name="id" size="20" maxlength="45"
-                   value="<?php if(isset($id)){
-                       echo htmlspecialchars($id, ENT_QUOTES, 'UTF-8');
-                   }?>"/>
 
-            　<?php if (isset($error['id'])){
-                if ($error['id'] == 'blank') :?>
+        <br>
+
+        <dt>ユーザID<span class="required">【必須】</span></dt>
+        <dd><input type="text" name="user_id" size="20" maxlength="45"
+                   value="<?php if(isset($user_id)){
+                       echo htmlspecialchars($user_id, ENT_QUOTES, 'UTF-8');
+                   }?>"/>
+<!--ユーザーIDが入力されていない場合、エラーメッセージを出力-->
+            　<?php if (isset($error['user_id'])){
+                if ($error['user_id'] == 'blank') :?>
                     <p class="error">*ユーザーIDを入力してください</p>
                 <?PHP endif;} ?>
+
+            <?php if (isset($error['user_id'])){
+                if($error['user_id'] == 'duplicate') :?>
+            <p class="error">指定されたユーザーIDはすでに登録されています</p>
+            <?php endif;} ?>
         </dd>
+
         <br>
+
         <dt>パスワード<span class="required">【必須】</span></dt>
         <dd><input type="password" name="password" size="10" maxlength="35"
                    value="<?php if(isset($password)){
                        echo htmlspecialchars($password, ENT_QUOTES, 'UTF-8');
                    }?>"/>
 
+<!--パスワードが4文字以上でない場合、エラーメッセージを出力-->
             <?php if (isset($error['password']))
             {if ($error['password'] == 'length') :?>
                 　<p class="error">*パスワードは4文字以上にしてください</p>
                 　<?PHP endif;} ?>
+
+<!--パスワードが入力されていない場合、エラーメッセージを出力-->
             　<?php if (isset($error['password'])){
                 if ($error['password'] == 'blank') :?>
                     <p class="error">*パスワードを設定してください</p>
                 <?PHP endif;} ?>
         </dd>
+
     </dl>
     <div><input type="submit" value="入力内容を確認する"></div>
 </form>
