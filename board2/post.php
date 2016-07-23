f233f7fc6be075db24d874f0f048b287af5ee5f2
shoko irie <shoko224335@gmail.com>
2016-07-24 02:55:14 +0900

    2016/07/24 02:54更新

--- board2/post.php
+++ board2/post.php
@@ -1,117 +1,71 @@
 <?php
 
-try {
-    session_start();
+try {session_start();
+    
+    require_once 'MySmarty.class.php';
     require_once 'dbconnect.php';
-    //セッション情報からmembersテーブルのIDとユーザーIDを取り出し格納
-    $user_id = $_SESSION['user_id'];
+
+    //セッション情報からmembersテーブルのIDとユーザーIDを取り出し変数に格納
     $id = $_SESSION['id'];
+    $user_id = $_SESSION['user_id'];
+
+    $smarty->assign('id', $id);
+    $smarty->assign('user_id', $user_id);
+
+    
+    if (isset($_POST['contents'])) {
+        //データベースへの接続を確立
+        $db = getDb();
+
+        // {投稿が空白になっていないか確認
+        if ($_POST['contents'] !== "") {
+            $stt = $db->prepare('INSERT INTO post (contents, user_id) VALUES (:contents, :user_id)');
+
+            //INSERT命令にポストデータとmemberテーブルのIDの内容をセット
+            $stt->bindValue(':contents', $_POST['contents']);
+            $stt->bindValue(':user_id', $id);
+            $stt->execute();
+
+        } else echo '*メッセージを入力してください';
+
+    }
 
     //ログイン状態であるか確認
-    if (isset($id) && isset($user_id)){
+    if (isset($id) && isset($user_id)) {
         //ログインしている
         //データベースへの接続を確立
         $db = getDb();
         $sql = $db->prepare("SELECT * FROM members WHERE user_id = '$user_id'");
         $sql->execute();
-        $members = $sql->fetch(PDO::FETCH_ASSOC);
-    } else {
-        //ログインしていない
-        header('Location: login.php');
-        exit();
-    }
-}   catch (PDOException $e){
-    die("エラーメッセージ:{$e->getMessage()}");
-    }
-?>
+        //$members = $sql->fetch(PDO::FETCH_ASSOC);
 
-<html>
-<head>
-    <meta charset="UTF-8">
-    <title>掲示板2</title>
-</head>
-<body>
-<table>
-<td>
-<form method="post" action="">
-        <p>メッセージをどうぞ！</p>
-        <input type="message" name="contents" size="75" maxlength="300"></dd>
-        <br>
-        <br>
-        <input type="submit" value="投稿する"></dd>
-</form>
-</td>
-<td>
-<form method="post" action="logout.php">
-    <p><input type="submit" value="ログアウトする"></p>
-</form>
-</td>
-</table>
-<?php
-
-require_once 'dbconnect.php';
-
-try {//データベースへの接続を確立
-    $db = getDb();
-
-    if (isset($_POST['contents'])) {
-
-        if ($_POST['contents'] !== "") {
-            $stt = $db->prepare('INSERT INTO post (contents, user_id) VALUES (:contents, :user_id)');
-            //INSERT命令にポストデータとmemberテーブルのIDの内容をセット
-            //どのユーザーの投稿かを認識
-            $stt->bindValue(':contents', $_POST['contents']);
-            $stt->bindValue(':user_id', $id);
-            $stt->execute();
-
-            //memberテーブルとpostテーブルを内部結合
-            //postのID、投稿内容、名前、postのユーザIDを選択
-            $join = $db->prepare('SELECT post.id as post_id, contents, name, post.user_id as post_user_id
+        //memberテーブルとpostテーブルを内部結合
+        //postのID、投稿内容、名前、postのユーザIDを選択
+        $join = $db->prepare('SELECT post.id as post_id, contents, name, post.user_id as post_user_id
             FROM post INNER JOIN members ON post.user_id = members.id ORDER BY post.id DESC');
-            $join->execute();
+        $join->execute();
 
-            while ($row = $join->fetch(PDO::FETCH_ASSOC)) {
-                echo "名前：";
-                echo $row['name'];
-                echo "<br>";
-                echo $row['contents'];
-                $post_id = $row['post_id'];
 
-                ?>
-                <?php if($id == $row['post_user_id']):?>
-                <table>
-                    <td>
-                        <form method="post" action="delete.php">
-                            <input type="hidden" name="id" value="<?php echo $id; ?>">
-                            <input type="hidden" name="post_id" value="<?php echo $post_id;?>">
-                            <input type="hidden" name="user_id" value="<?php echo $user_id; ?>">
-                            <input type="submit" name="delete" value="削除する">
-                        </form>
-                    </td>
-                <?php endif;?>
+        //配列を定義し、初期化
+        $items_list = array();
+        while ($row = $join->fetch(PDO::FETCH_ASSOC)) {
+                $items_list[] = array('post_id' => $row['post_id'], 'name' => $row['name'],
+                'contents' => $row['contents'], 'post_user_id' => $row['post_user_id']);
+        }
 
-                <?php if($id == $row['post_user_id']):?>
-                    <td>
-                        <form method="post" action="edit.php">
-                            <input type="hidden" name="id" value="<?php echo $id; ?>">
-                            <input type="hidden" name="post_id" value="<?php echo $post_id;?>">
-                            <input type="hidden" name="user_id" value="<?php echo $user_id; ?>">
-                            <input type="submit" name="edit" value="編集する">
-                        </form>
-                    </td>
-                </table>
-                <?php endif;?>
+        $smarty->assign('items', $items_list);
 
-                <?php echo "<br>";
-                echo "<br>"; ?>
 
-            <?php }
-        }else print "*メッセージを入力してください";
-    }
+    }else {
+            //ログインしていない
+            header('Location: login.php');
+            exit();
+        }
+    
+
+
 } catch (PDOException $e){
   die("エラーメッセージ:{$e->getMessage()}");
   }
 
-?>
-</body>
-</html>
+$smarty->display('post.tpl');
---
+++ board2/post.tpl
@@ -0,0 +1,63 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>掲示板2</title>
+</head>
+
+<body>
+
+
+<!--投稿フォームの作成-->
+<table>
+
+        <form method="post" action="post.php">
+            <p>メッセージをどうぞ！</p>
+            <input type="message" name="contents" size="75" maxlength="300"></dd>
+            <br>
+            <br>
+            <input type="submit" value="投稿する"></dd>
+        </form>
+    </td>
+
+    <td>
+        <form method="post" action="logout.php">
+            <p><input type="submit" value="ログアウトする"></p>
+        </form>
+    </td>
+</table>
+
+{if isset($id) && isset($user_id)}
+
+            {foreach from=$items key=myId item=i}
+                <br>
+                {$i.name}
+                <br>
+                {$i.contents}
+                <br>
+
+                       {if $id == $i.post_user_id}
+                        <table>
+                        <td>
+                        <form method="post" action="delete.php">
+                        <input type="hidden" name="id" value="{$id}">
+                        <input type="hidden" name="post_id" value="{$post_id}">
+                        <input type="hidden" name="user_id" value="{$user_id}">
+                        <input type="submit" name="delete" value="削除する">
+                        </form>
+                        </td>
+
+                        <td>
+                        <form method="post" action="edit.php">
+                        <input type="hidden" name="id" value="{$id}">
+                        <input type="hidden" name="post_id" value="{$post_id}">
+                        <input type="hidden" name="user_id" value="{$user_id}">
+                        <input type="submit" name="edit" value="編集する">
+                        </form>
+                        </td>
+                        </table>
+                        {/if}
+            {/foreach}
+{/if}
+
+</body>
+</html>
