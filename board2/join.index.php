f2dd3a7ddd90a09ff12104f83d6146b89e9f59d7
shoko irie <shoko224335@gmail.com>
2016-07-24 17:21:19 +0900

    2016/07/24 17:20更新

--- board2/join.index.php
+++ board2/join.index.php
@@ -2,13 +2,9 @@
 
 try {session_start();
     //データベースに接続
+    require_once 'MySmarty.class.php';
     require_once 'dbconnect.php';
-
-
-    //htemlspecialcharsのショートカットを作成
-    function h($value){
-        return htmlspecialchars($value, ENT_QUOTES, 'UTF-8');
-    }
+    
     //データベースへの接続を確立
     $db = getDb();
 
@@ -17,21 +13,30 @@ try {session_start();
         $name = $_POST['name'];
         $user_id = $_POST['user_id'];
         $password = $_POST['password'];
+
+        $smarty->assign('name', $name);
+        $smarty->assign('user', $user_id);
+        $smarty->assign('name', $password);
+
         //フォームが送信されているか確認
         //送信されている場合、次の処理に進む
         if (!empty($_POST)) {
             //送信されていた場合、それぞれのフォーム欄が空白になっていないか、パスワードは4文字以上か確認
             if ($name == '') {
                 $error['name'] = 'blank';
+                $smarty->assign('error_name', $error['name']);
             }
             if ($user_id == '') {
                 $error['user_id'] = 'blank';
+                $smarty->assign('error_user_id_blank', $error['user_id']);
             }
             if (strlen($password) < 4) {
                 $error['password'] = 'length';
+                $smarty->assign('error_password_length', $error['password']);
             }
             if ($password == '') {
                 $error['password'] = 'blank';
+                $smarty->assign('error_password_blank', $error['password']);
             }
 
             //ユーザーIDが重複していないか確認
@@ -42,6 +47,7 @@ try {session_start();
                 $count = $stt->rowCount();
                 if ($count > 0){
                     $error['user_id'] = 'duplicate';
+                    $smarty->assign('error_user_id_duplicate', $error['user_id']);
                 }
             }
             //エラー項目がない場合、セッション情報をcheck.phpに送信
@@ -50,6 +56,7 @@ try {session_start();
                 $_SESSION['name'] = $name;
                 $_SESSION['user_id'] = $user_id;
                 $_SESSION['password'] = $password;
+
                 header('Location: check.php');
                 exit();
             }
@@ -58,76 +65,5 @@ try {session_start();
 }catch (PDOException $e){
     die("エラーメッセージ:{$e->getMessage()}");
 }
-?>
 
-<html>
-<head>
-    <meta charset="utf-8">
-    <title>会員登録</title>
-</head>
-<body>
-
-<!--入力フォームを作成-->
-<!--入力内容を常に再現-->
-<!--エラー項目をユーザーに知らせる-->
-<p>次のフォームに必要事項をご記入ください。</p>
-<form method="post" action=''>
-    <dl>
-        <dt>お名前<span class="required">【必須】</span></dt>
-        <dd><input type="text" name="name" size="35" maxlength="255"
-                   value="<?php if(isset($name)){
-                       echo h($name, ENT_QUOTES, 'UTF-8');
-                   }?>"/>
-
-            <!--名前が入力されていない場合、エラーメッセージを出力-->
-            <?php if (isset($error['name'])){
-                if($error['name'] == 'blank') :?>
-                    　<p class="error">*お名前を入力してください</p>
-                    　<?PHP endif;} ?>
-        </dd>
-
-        <br>
-
-        <dt>ユーザID<span class="required">【必須】</span></dt>
-        <dd><input type="text" name="user_id" size="20" maxlength="45"
-                   value="<?php if(isset($user_id)){
-                       echo h($user_id, ENT_QUOTES, 'UTF-8');
-                   }?>"/>
-<!--ユーザーIDが入力されていない場合、エラーメッセージを出力-->
-            　<?php if (isset($error['user_id'])){
-                if ($error['user_id'] == 'blank') :?>
-                    <p class="error">*ユーザーIDを入力してください</p>
-                <?PHP endif;} ?>
-
-            <?php if (isset($error['user_id'])){
-                if($error['user_id'] == 'duplicate') :?>
-            <p class="error">指定されたユーザーIDはすでに登録されています</p>
-            <?php endif;} ?>
-        </dd>
-
-        <br>
-
-        <dt>パスワード<span class="required">【必須】</span></dt>
-        <dd><input type="password" name="password" size="10" maxlength="35"
-                   value="<?php if(isset($password)){
-                       echo h($password, ENT_QUOTES, 'UTF-8');
-                   }?>"/>
-
-<!--パスワードが4文字以上でない場合、エラーメッセージを出力-->
-            <?php if (isset($error['password']))
-            {if ($error['password'] == 'length') :?>
-                　<p class="error">*パスワードは4文字以上にしてください</p>
-                　<?PHP endif;} ?>
-
-<!--パスワードが入力されていない場合、エラーメッセージを出力-->
-            　<?php if (isset($error['password'])){
-                if ($error['password'] == 'blank') :?>
-                    <p class="error">*パスワードを設定してください</p>
-                <?PHP endif;} ?>
-        </dd>
-
-    </dl>
-    <div><input type="submit" value="入力内容を確認する"></div>
-</form>
-</body>
-</html>
+$smarty->display('join.index.tpl');
